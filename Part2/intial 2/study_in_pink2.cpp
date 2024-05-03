51f44ce55e5cbb415e79e80312c23af1ee14be65
Hồ Nhật Trị <honhattri2004@gmail.com>
2024-05-03 20:20:16 +0700

    implement robot

--- Part2/intial 2/study_in_pink2.cpp
+++ Part2/intial 2/study_in_pink2.cpp
@@ -6,8 +6,43 @@
 /// DO NOT modify any parameters in the functions.
 ////////////////////////////////////////////////////////////////////////
 
+class Robot : public MovingObject{
+    protected:
+        RobotType robot_type;
+        long long NUM = INT_MAX; 
+        BaseItem *items;
+        Position intPos; // inital position of Robot
+    public:
+        string robotType[4] = {"C", "S", "W", "SW"};
+        Robot(int index, const Position& int_pos, Map*map, string name)
+        : MovingObject(index, int_pos, map, name ), items(items) {}
+        void setRobotType(RobotType robot_type){
+            this->robot_type = robot_type;
+        }
+        RobotType getRobotType(){
+            return this->robot_type;
+        }
+        void setItems(BaseItem *items){
+            this->items = items;
+        }
+        BaseItem *getItems(){
+            return this->items;
+        }
+        ~Robot();
+       
+};
+class RobotC : public Robot{
 
+};
+class RobotS : public Robot{
 
+};
+class RobotW : public Robot{
+
+};
+class RobotSW : public Robot{
+  
+};
 ////////////////////////////////////////////////
 /// END OF STUDENT'S ANSWER
 ////////////////////////////////////////////////
